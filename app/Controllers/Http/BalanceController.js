40dd4af38baf46cce23d71408a4e61b984a7a740
Eduardo Augusto Gomes de Oliveira <eduaugusto10@gmail.com>
2021-11-23 23:06:00 -0300

    direction rules add

--- app/Controllers/Http/BalanceController.js
+++ app/Controllers/Http/BalanceController.js
@@ -254,12 +254,14 @@ class BalanceController {
     let close = [];
     let closeOrder;
     for (let i = 0; i < balance.rows.length; i++) {
-      closeOrder = await Order.query()
-        .where("order_id", balance.rows[i].ticket)
-        .andWhere("direction", "0")
-        .orderBy("id", "desc")
-        .fetch();
-      if (closeOrder.rows.length > 0) close.push(closeOrder.rows[0]);
+      if (balance.rows[i].direction === 1) {
+        closeOrder = await Order.query()
+          .where("order_id", balance.rows[i].ticket)
+          .andWhere("direction", "0")
+          .orderBy("id", "desc")
+          .fetch();
+        if (closeOrder.rows.length > 0) close.push(closeOrder.rows[0]);
+      }
     }
     return { balance, close };
   }
